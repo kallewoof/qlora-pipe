79d0180eff9e04139d1d64b551143604bfd56caa
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2024-11-14 14:21:41 +0900

    dataloader: add token counter and total token sum
    add 1-dim poly fit eta based on tokens & steps
    
    include eval times in eta calculation
    
    rename etas to elapsed_hist
    
    qol: write out a UTF-8 plot on screen showing eval loss history, if plotille is installed
    
    colorize the plot (red=worsen green=improve)
    
    steps, not epochs, for utfplot
    
    support LoRA+ optimizer
    
    feature: eval proportion for auto calculating eval_steps
    
    print out train time at each switch eval/train
    
    feature: --append
    
    The --append flag lets the user provide a new set of training data that is trained on top of a checkpoint from a previous run.
    
    WIP token based eta
    
    wip token based eta
    
    wip
    
    fix multi epoch eta
    
    eval_after_last_step flag
    
    add eval eta to global eta
    
    use count_str for token print
    
    mb consideration
    
    revert
    
    skip ggufs in merge_lora.py
    
    save all losses, not just the best one
    
    handle resume case, and tweak exprs
    
    fix code
    
    bug: count all tokens across all ranks
    
    formatting
    
    cleanup
    
    fixed above commit
    
    total tokens on rank 0 only
    
    merge_lora: also skip any other .safetensors files not in shards
    
    cleanup
    
    wip token based eta
    
    restore gemma 2 thing
    
    key log merge
    
    dont divide by num_stages in token counter

--- dataloader.py
+++ dataloader.py
@@ -5,25 +5,24 @@ import sys
 
 sys.path.insert(0, os.path.abspath('axolotl/src'))
 
-import accelerate
 import torch
 import transformers
 from deepspeed import comm as dist
 from torch.utils.data import DataLoader
 
 from axolotl.utils.collators import DataCollatorForSeq2Seq
-
-
-# from utils import *
+from utils import count_str, is_main_process
 
 
 # A100 wants padding to multiple of 64, other cards are efficient with smaller, so just do 64
 PAD_TO_MULTIPLE = 64
 
 
-def split_batch(batch, pieces):
+def split_batch(batch, pieces, rank=None):
     example_tuple, labels = batch
-    print(f'before GAS splitting, input_ids shape: {example_tuple[0].shape}, total tokens: {example_tuple[0].numel()}')
+    if rank is not None or is_main_process():
+        r = f'rank {rank}: ' if rank is not None else ''
+        print(f'{r}before GAS splitting, input_ids shape: {example_tuple[0].shape}, total tokens: {example_tuple[0].numel()}')
     split_size = example_tuple[0].size(0) // pieces
     split_examples = zip(*(torch.split(tensor, split_size) for tensor in example_tuple))
     return [(ex, None) for ex in split_examples]
@@ -117,8 +116,10 @@ class DistributedBatchSamper(torch.utils.data.Sampler):
         # make sure the largest batch comes first to OOM sooner rather than later
         largest_global_batch = 0
         max_tokens = 0
+        total_tokens = 0
         for global_batch_idx, batch in enumerate(global_batches):
             total_batch_tokens = batch_size_tokens_after_padding(batch)
+            total_tokens += total_batch_tokens
             if total_batch_tokens > max_tokens:
                 max_tokens = total_batch_tokens
                 largest_global_batch = global_batch_idx
@@ -126,6 +127,10 @@ class DistributedBatchSamper(torch.utils.data.Sampler):
             global_batches[largest_global_batch],
             global_batches[0],
         )
+        tally_tokens = torch.tensor(total_tokens, device='cuda')
+        dist.all_reduce(tally_tokens)
+        self.total_tokens = tally_tokens
+        print(f"rank {rank}: {count_str(self.total_tokens)} tokens/epoch")
 
         batches_for_this_rank = [
             global_batch[self.rank : len(global_batch) : self.num_replicas] for global_batch in global_batches
@@ -174,6 +179,8 @@ class PipelineDataLoader:
         self.batch_size_tokens = batch_size_tokens
         self.gradient_accumulation_steps = gradient_accumulation_steps
         self.pad_to_multiple_of = pad_to_multiple_of
+        self.skip_batches = 0
+        self.rank = data_parallel_rank
         self.data_sampler = DistributedBatchSamper(
             dataset=dataset,
             batch_size=self.batch_size,
@@ -192,11 +199,15 @@ class PipelineDataLoader:
         self._create_dataloader()
         self.data = self._pull_batches_from_dataloader()
 
+        self.samplelogger = open(f"/llm/logs/samples_{data_parallel_rank}.txt", "w")
+        self.reset()
+
     def reset(self):
         self.epoch = 1
         self.num_batches_pulled = 0
         self.next_micro_batch = None
         self.data = self._pull_batches_from_dataloader()
+        self.processed_tokens = 0
 
     def __iter__(self):
         return self
@@ -222,8 +233,13 @@ class PipelineDataLoader:
 
     def _pull_batches_from_dataloader(self):
         for batch in self.dataloader:
+            if self.skip_batches > 0:
+                self.skip_batches -= 1
+                continue
             self.num_batches_pulled += 1
-            yield from split_batch(batch, self.gradient_accumulation_steps)
+            for micro_batch in split_batch(batch, self.gradient_accumulation_steps):
+                self.samplelogger.write(f"Next batch:\n********************************\n{'\n********************************\n'.join(self.tokenizer.decode(b) for b in micro_batch[0][0])}\n")
+                yield micro_batch
 
     def _create_dataloader(self):
         data_collator = DataCollatorForSeq2Seq(self.tokenizer, pad_to_multiple_of=self.pad_to_multiple_of)
@@ -243,6 +259,10 @@ class PipelineDataLoader:
             if rejected_examples:
                 examples = combine_piecewise(examples, rejected_examples, self.gradient_accumulation_steps)
             batch = data_collator(examples)
+
+            input_token_count = batch['input_ids'].ne(self.tokenizer.pad_token_id).sum().item()
+            self.processed_tokens += input_token_count
+
             # input to pipeline is (input_ids, attention_mask, labels)
             # this needs to return (features, labels)
             # it is OK if labels is None (the model just returns the loss anyway)
@@ -263,11 +283,14 @@ class PipelineDataLoader:
 
     def load_state_dict(self, state_dict):
         self.epoch = state_dict['epoch']
+        self.processed_tokens = (self.epoch - 1) * self.data_sampler.total_tokens
         # -1 because by preloading the next micro_batch, it's always going to have one more batch
         # pulled than the actual number of batches iterated by the caller.
         self.num_batches_pulled = state_dict['num_batches_pulled'] - 1
         self._create_dataloader()
-        self.dataloader = accelerate.skip_first_batches(self.dataloader, self.num_batches_pulled)
+        # TODO: see if we can route this through accelerate still and still get the collate_fn counting correctly
+        self.skip_batches = self.num_batches_pulled
+        # self.dataloader = accelerate.skip_first_batches(self.dataloader, self.num_batches_pulled)
         self.data = self._pull_batches_from_dataloader()
         # Recreate the dataloader after the first pass so that it won't skip
         # batches again (we only want it to skip batches the first time).
